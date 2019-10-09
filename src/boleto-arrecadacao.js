4aa28b640f974239d68486aea3bd18c82579dc9a
Sander <alex.rocha@zoop.co>
2019-10-08 23:55:17 -0300

    Adicionando suporte a validação bloco a bloco para boleto de arrecadação

--- src/boleto-arrecadacao.js
+++ src/boleto-arrecadacao.js
@@ -17,29 +17,54 @@ export function boletoArrecadacaoCodigoBarras(codigo) {
 
 export function boletoArrecadacaoLinhaDigitavel(codigo, validarBlocos = false) {
   const cod = clearMask(codigo);
-  if (!/^[0-9]{48}$/.test(cod) || Number(cod[0]) !== 8) return false;
   const validDV = boletoArrecadacaoCodigoBarras(convertToBoletoArrecadacaoCodigoBarras(cod));
-  if (!validarBlocos) return validDV;
-  const codigoMoeda = Number(cod[2]);
-  let modulo;
-  if (codigoMoeda === 6 || codigoMoeda === 7) modulo = modulo10;
-  else if (codigoMoeda === 8 || codigoMoeda === 9) modulo = modulo11;
-  else return false;
-  const blocos = Array.from({ length: 4 }, (v, index) => {
-    const start = (11 * (index)) + index;
-    const end = (11 * (index + 1)) + index;
-    return {
-      num: cod.substring(start, end),
-      DV: cod.substring(end, end + 1),
-    };
-  });
-  const validBlocos = blocos.every(e => modulo(e.num) === Number(e.DV));
-  return validBlocos && validDV;
+  if (validarBlocos) {
+    const codigoMoeda = Number(cod[2]);
+    let modulo;
+    if (codigoMoeda === 6 || codigoMoeda === 7) modulo = modulo10;
+    else if (codigoMoeda === 8 || codigoMoeda === 9) modulo = modulo11;
+    else return false;
+
+    let validBlocos;
+
+    if (cod.length >= 12) {
+      const num = cod.substring(0, 11);
+      const DV = cod.substring(11, 12);
+      validBlocos = modulo(num) === Number(DV);
+    }
+
+    if (cod.length >= 24) {
+      const num = cod.substring(12, 23);
+      const DV = cod.substring(23, 24);
+      validBlocos = validBlocos && modulo(num) === Number(DV);
+    }
+
+    if (cod.length >= 36) {
+      const num = cod.substring(24, 35);
+      const DV = cod.substring(35, 36);
+      validBlocos = validBlocos && modulo(num) === Number(DV);
+    }
+
+    if (cod.length === 48) {
+      const blocos = Array.from({ length: 4 }, (v, index) => {
+        const start = (11 * (index)) + index;
+        const end = (11 * (index + 1)) + index;
+        return {
+          num: cod.substring(start, end),
+          DV: cod.substring(end, end + 1),
+        };
+      });
+      validBlocos = blocos.every(e => modulo(e.num) === Number(e.DV)) && validDV;
+    }
+
+    return validBlocos;
+  }
+  if (!/^[0-9]{48}$/.test(cod) || Number(cod[0]) !== 8) return false;
+  return validDV;
 }
 
 export function boletoArrecadacao(codigo, validarBlocos = false) {
   const cod = clearMask(codigo);
   if (cod.length === 44) return boletoArrecadacaoCodigoBarras(cod);
-  if (cod.length === 48) return boletoArrecadacaoLinhaDigitavel(codigo, validarBlocos);
-  return false;
+  return boletoArrecadacaoLinhaDigitavel(codigo, validarBlocos);
 }
